8e3a0e12231aa598343233db1752a6a7be129b3f
benjamin-maynard <383838-github@ea.maynard.io>
2018-12-16 15:46:47 +0000

    Fixed multiple Slack alerting issues.

--- resources/perform-backup.sh
+++ resources/perform-backup.sh
@@ -5,29 +5,27 @@
 has_failed=false
 
 
+
+
 # Perform the database backup. Put the output to a variable. If successful upload the backup to S3, if unsuccessful print an entry to the console and the log, and set has_failed to true.
-if sqloutput=$(mysqldump -u $TARGET_DATABASE_USER -h $TARGET_DATABASE_HOST -p$TARGET_DATABASE_PASSWORD -P $TARGET_DATABASE_PORT $TARGET_DATABASE_NAME 2>&1 > /tmp/$AWS_BUCCKET_BACKUP_NAME)
+if sqloutput=$(mysqldump -u $TARGET_DATABASE_USER -h $TARGET_DATABASE_HOST -p$TARGET_DATABASE_PASSWORD -P $TARGET_DATABASE_PORT $TARGET_DATABASE_NAMES 2>&1 > /tmp/$AWS_BUCCKET_BACKUP_NAME)
 then
     
-    echo -e "Database backup successfully completed for $TARGET_DATABASE_NAME at $(date +'%d-%m-%Y %H:%M:%S')."
+    echo -e "Database backup successfully completed for $TARGET_DATABASE_NAMES at $(date +'%d-%m-%Y %H:%M:%S')."
 
     # Perform the upload to S3. Put the output to a variable. If successful, print an entry to the console and the log. If unsuccessful, set has_failed to true and print an entry to the console and the log
     if awsoutput=$(aws s3 cp /tmp/$AWS_BUCCKET_BACKUP_NAME s3://$AWS_BUCKET_NAME$AWS_BUCKET_BACKUP_PATH$AWS_BUCCKET_BACKUP_NAME 2>&1)
     then
-        echo -e "Database backup successfully uploaded for $TARGET_DATABASE_NAME at $(date +'%d-%m-%Y %H:%M:%S')."
+        echo -e "Database backup successfully uploaded for $TARGET_DATABASE_NAMES at $(date +'%d-%m-%Y %H:%M:%S')."
     else
-        echo -e "Database backup failed to upload for $TARGET_DATABASE_NAME at $(date +'%d-%m-%Y %H:%M:%S'). Error: $awsoutput" | tee -a /tmp/aws-database-backup.log
+        echo -e "Database backup failed to upload for $TARGET_DATABASE_NAMES at $(date +'%d-%m-%Y %H:%M:%S'). Error: $awsoutput" | tee -a /tmp/aws-database-backup.log
         has_failed=true
     fi
 
 else
-    echo -e "Database backup FAILED for $TARGET_DATABASE_NAME at $(date +'%d-%m-%Y %H:%M:%S'). Error: $sqloutput" | tee -a /tmp/aws-database-backup.log
+    echo -e "Database backup FAILED for $TARGET_DATABASE_NAMES at $(date +'%d-%m-%Y %H:%M:%S'). Error: $sqloutput" | tee -a /tmp/aws-database-backup.log
     has_failed=true
 fi
-
-
-# Put the contents of the database backup logs into a variable
-logcontents=`cat /tmp/aws-database-backup.log`
 
 
 # Check if any of the backups have failed. If so, exit with a status of 1. Otherwise exit cleanly with a status of 0.
@@ -37,6 +35,10 @@ then
     # If Slack alerts are enabled, send a notification alongside a log of what failed
     if [ "$SLACK_ENABLED" = true ]
     then
+        # Put the contents of the database backup logs into a variable
+        logcontents=`cat /tmp/aws-database-backup.log`
+
+        # Send Slack alert
         /slack-alert.sh "One or more backups on database host $TARGET_DATABASE_HOST failed. The error details are included below:" "$logcontents"
     fi
 
--- resources/slack-alert.sh
+++ resources/slack-alert.sh
@@ -2,12 +2,12 @@
 
 
 # Check if there is any value in $2. If so, post an entry to the Slack channel with log information. If not, send a general message that all databases successfully completed
-if [ -v "$2"]
+if [ "$(printf '%s' "$2")" == '' ]
 then
 PAYLOAD="payload={\"channel\": \"$SLACK_CHANNEL\", \"username\": \"$SLACK_USERNAME\", \"text\": \"$1\", \"icon_emoji\": \":slack:\"}"
 else
-PAYLOAD="payload={\"channel\": \"$SLACK_CHANNEL\", \"username\": \"$SLACK_USERNAME\", \"text\": \"$1\`\`\`$2\`\`\`\", \"icon_emoji\": \":slack:\"}"
+PAYLOAD="payload={\"channel\": \"$SLACK_CHANNEL\", \"username\": \"$SLACK_USERNAME\", \"text\": \"$1\`\`\`$(echo $2 | sed "s/\"/'/g")\`\`\`\", \"icon_emoji\": \":slack:\"}"
 fi
 
-
-curl -X POST --data-urlencode "$PAYLOAD" "$SLACK_WEBHOOK_URL"
+# Send Slack message
+curl -s -X POST --data-urlencode "$PAYLOAD" "$SLACK_WEBHOOK_URL" > /dev/null
